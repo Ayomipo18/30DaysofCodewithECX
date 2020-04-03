fd8fa0fc288287f3f6ebb2255e20e2ce6ab48145
Ayomipo Solaja <ayomiposolaja@gmail.com>
2020-04-03 21:17:54 +0100

    Email Validation with Regular Expression

--- Day10_Email_Validation.js
+++ Day10_Email_Validation.js
@@ -2,3 +2,5 @@ function validateEmail(email){
    var emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
    return emailPattern.test(email); 
  } 
+
+ console.log(validateEmail('ayomipo@gmail.com'))
