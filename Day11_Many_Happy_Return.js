3f74edbc3c8a00870726f076ddf5e63f856a4610
Ayomipo Solaja <ayomiposolaja@gmail.com>
2020-04-06 11:10:47 +0100

    a program that takes in a date of birth of a person and returns the

---
+++ Day11_Many_Happy_Return.js
@@ -0,0 +1,15 @@
+function nextBirthday(dob){
+
+let birthday = new Date(dob);
+let today = new Date();
+birthday.setFullYear(today.getFullYear());
+
+if (today > birthday) {
+  birthday.setFullYear(today.getFullYear() + 1);
+}
+
+return Math.ceil((birthday - today) / (1000*60*60*24)) + ' days';
+}
+
+//Date of Birth format : MM-DD-YY
+nextBirthday("10-13-1998")
